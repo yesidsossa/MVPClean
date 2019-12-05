95e97624aecd8a001c9454abd5e6d1361d373625
Yesid Hernandez <yhernandez@condorlabs.io>
2019-12-05 17:52:34 -0500

    Arquitectura

---
+++ ios-tinder-cat/TinderCat/Pods/Target Support Files/Pods-TinderCat-TinderCatUITests/Pods-TinderCat-TinderCatUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TinderCat_TinderCatUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TinderCat_TinderCatUITests
+@end
---
+++ ios-tinder-cat/TinderCat/Pods/Target Support Files/Pods-TinderCatTests/Pods-TinderCatTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TinderCatTests : NSObject
+@end
+@implementation PodsDummy_Pods_TinderCatTests
+@end
